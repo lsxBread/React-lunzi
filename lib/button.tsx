b49c015ce9f4bb4a1a7cdcc624888ae43ca178c8
shixun liu <shixun.liu1023@gmail.com>
2019-04-21 22:52:42 +1000

    add externals in webpack config

---
+++ lib/button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Button() {
+    return (
+        <div>
+            testing
+        </div>
+    )
+}
+
+export default Button
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,1 +1,8 @@
-console.log('hi123123')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from './button';
+
+ReactDOM.render(
+    <Button />,
+    document.getElementById('root')
+)
