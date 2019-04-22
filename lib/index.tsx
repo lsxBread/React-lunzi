9876a30e4895cf9285c4fd649baf6e422ca66e0e
Shixun Liu <shixun.liu1023@gmail.com>
2019-04-22 12:06:40 +1000

    add scss for icon

--- lib/index.tsx
+++ lib/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import Icon from './icon';
 
 ReactDOM.render(
-    <Icon name="youtube"/>,
+    <div>
+      <Icon name="youtube"/>
+      <Icon name="alipay"/>
+      <Icon name="wechat"/>
+    </div>,
     document.getElementById('root')
 )
